556ba77ec2367c9556e0eb82763b1d505b0bab39
shobhan <shobhansuri@gmail.com>
2014-07-28 16:00:48 +0530

    name added to listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,4 +10,6 @@ has_attached_file :image, :styles => { :medium => "200x", :thumb => "160x160!" }
    	validates :name, :description, :price, presence: true
    	validates :price, numericality: { greater_than: 0}
    	validates_attachment_presence :image
+
+   belongs_to :user
 end
