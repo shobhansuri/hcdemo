9340ec0ecf60c9f43b5ec9f0ac118ce30548fbf6
shobhan <shobhansuri@gmail.com>
2014-08-03 21:55:23 +0530

    seller dashboad

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,8 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-has_attached_file :image, :styles => { :medium => "200x", :thumb => "160x160!" }, :default_url => "noimage.png"
+has_attached_file :image, :styles => { :medium => "300x", :thumb => "160x160" }, :default_url => "noimage.png"
 else
-has_attached_file :image, :styles => { :medium => "200x", :thumb => "160x160!" }, :default_url => "noimage.png",:storage => :dropbox,
+has_attached_file :image, :styles => { :medium => "300x", :thumb => "160x160" }, :default_url => "noimage.png",:storage => :dropbox,
    			 :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
    			 :path => ":style/:id_:filename"
    	end
