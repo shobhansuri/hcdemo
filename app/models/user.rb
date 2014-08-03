64f6b1bc5118dd098986f4fd1de86f5f36ccac34
shobhan <shobhansuri@gmail.com>
2014-08-03 08:52:50 +0530

    added college field

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
     validates :name, presence: true
+     validates :colg, presence: true
 
    has_many :listings, dependent: :destroy
 
